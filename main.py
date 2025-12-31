6bf69c81f96e0a344e8671f6843bc05f6ace0029
Marlus Araujo <m@marlus.com>
2025-12-31 16:28:07 -0500

    initial files

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+# Load environment variables from .env file
+load_dotenv()
+
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+
+app = ApplicationBuilder().token(os.getenv("TELEGRAM_BOT_TOKEN")).build()
+
+app.add_handler(CommandHandler("hello", hello))
+
+app.run_polling()
